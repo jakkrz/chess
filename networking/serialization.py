c2ceb6c165787ccaf178456ddd84f00b0335ac4d
jakkrz <jkranz.business@gmail.com>
2023-03-22 19:10:04 +0100

    implemented move serialization

--- networking/serialization.py
+++ networking/serialization.py
@@ -1,3 +1,4 @@
+from typing import Type, Dict
 from color import Color
 from move import (
     Move,
@@ -9,16 +10,128 @@ from move import (
 )
 from game_state.game_state import GameState
 from parser.fen_parser import FenParser
+from coordinate import Coordinate
+from notation import (
+    get_piece_by_character,
+    get_character_by_piece,
+    get_castling_move_by_character,
+    get_character_by_castling_move
+)
+from piece import Piece
+
+
+MOVE_TYPE_SPECIFIERS: Dict[Type[Move], bytes] = {
+    BasicMove: b"b",
+    PawnDoubleMove: b"d",
+    EnPassantMove: b"e",
+    PromotionMove: b"p",
+    CastlingMove: b"c",
+}
+
+
+def serialize_coordinate(coordinate: Coordinate) -> bytes:
+    return repr(coordinate).encode()
+
+
+def deserialize_coordinate(coordinate_bytes: bytes) -> Coordinate:
+    return Coordinate.from_string(coordinate_bytes.decode(encoding="ascii"))
+
+
+def serialize_piece(piece: Piece) -> bytes:
+    return get_character_by_piece(piece).encode()
+
+
+def deserialize_piece(piece_bytes: bytes) -> Piece:
+    return get_piece_by_character(piece_bytes.decode(encoding="ascii"))
 
 
 def serialize_move(move: Move) -> bytes:
-    # TODO
-    pass
+    type_specifier = MOVE_TYPE_SPECIFIERS[type(move)]
+
+    if isinstance(move, BasicMove):
+        return type_specifier + serialize_basic_move(move)
+    elif isinstance(move, PawnDoubleMove):
+        return type_specifier + serialize_pawn_double_move(move)
+    elif isinstance(move, PromotionMove):
+        return type_specifier + serialize_promotion_move(move)
+    elif isinstance(move, CastlingMove):
+        return type_specifier + serialize_castling_move(move)
+    elif isinstance(move, EnPassantMove):
+        return type_specifier + serialize_en_passant_move(move)
+    else:
+        raise ValueError("invalid move type")
+
+
+def serialize_basic_move(move: BasicMove) -> bytes:
+    return serialize_coordinate(move.source_square) + serialize_coordinate(move.target_square)
+
+
+def serialize_pawn_double_move(move: PawnDoubleMove) -> bytes:
+    return serialize_coordinate(move.source_square) + serialize_coordinate(move.target_square)
+
+
+def serialize_castling_move(move: CastlingMove) -> bytes:
+    return get_character_by_castling_move(move).encode()
+
+
+def serialize_en_passant_move(move: EnPassantMove) -> bytes:
+    return serialize_coordinate(move.source_square) + serialize_coordinate(move.target_square)
+
+
+def serialize_promotion_move(move: PromotionMove) -> bytes:
+    return serialize_coordinate(move.source_square) + serialize_coordinate(move.target_square) + serialize_piece(Piece(Color.WHITE, move.promote_to))
 
 
 def deserialize_move(move_bytes: bytes) -> Move:
-    # TODO
-    pass
+    initial_byte = move_bytes[0:1]
+    remaining_bytes = move_bytes[1:]
+
+    move_type = get_dict_index_for_value(MOVE_TYPE_SPECIFIERS, initial_byte)
+
+    if move_type is None:
+        raise ValueError("Invalid move type specifier")
+
+    if move_type is BasicMove:
+        return deserialize_basic_move(remaining_bytes)
+    elif move_type is PromotionMove:
+        return deserialize_promotion_move(remaining_bytes)
+    elif move_type is EnPassantMove:
+        return deserialize_en_passant_move(remaining_bytes)
+    elif move_type is PawnDoubleMove:
+        return deserialize_pawn_double_move(remaining_bytes)
+    elif move_type is CastlingMove:
+        return deserialize_castling_move(remaining_bytes)
+    else:
+        raise ValueError("invalid move type")
+        
+
+def get_dict_index_for_value(_dict, value):
+    for key, value_in_dict in _dict.items():
+        if value_in_dict == value:
+            return key
+
+    return None
+
+    
+def deserialize_basic_move(move_bytes: bytes) -> BasicMove:
+    return BasicMove(deserialize_coordinate(move_bytes[0:2]), deserialize_coordinate(move_bytes[2:4]))
+
+
+def deserialize_pawn_double_move(move_bytes: bytes) -> PawnDoubleMove:
+    return PawnDoubleMove(deserialize_coordinate(move_bytes[0:2]), deserialize_coordinate(move_bytes[2:4]))
+
+
+def deserialize_castling_move(move_bytes: bytes) -> CastlingMove:
+    return get_castling_move_by_character(move_bytes.decode(encoding="ascii"))
+
+
+def deserialize_en_passant_move(move_bytes: bytes) -> EnPassantMove:
+    return EnPassantMove(deserialize_coordinate(move_bytes[0:2]), deserialize_coordinate(move_bytes[2:4]))
+
+
+def deserialize_promotion_move(move_bytes: bytes) -> PromotionMove:
+    # slice of [4:5] to avoid returning int
+    return PromotionMove(deserialize_coordinate(move_bytes[0:2]), deserialize_coordinate(move_bytes[2:4]), deserialize_piece(move_bytes[4:5]).piece_type)
 
 
 def serialize_game_state(game_state: GameState) -> bytes:
--- notation.py
+++ notation.py
@@ -1,5 +1,8 @@
 from piece import Piece, PieceType
 from color import Color
+from move import CastlingMove
+from castling_side import CastlingSide
+
 
 def get_character_by_piece(piece: Piece) -> str:
     result = ""
@@ -21,6 +24,7 @@ def get_character_by_piece(piece: Piece) -> str:
         return result.upper()
 
     return result
+
 
 def get_piece_by_character(character: str) -> Piece:
     color = Color.WHITE if character.isupper() else Color.BLACK
@@ -45,11 +49,11 @@ def get_piece_by_character(character: str) -> Piece:
     return Piece(color, piece_type)
 
 
-def _get_char_number_in_alphabet(char):
+def _get_char_number_in_alphabet(char: str):
     return ord(char) - 97
 
 
-def get_file_by_char(char) -> int:
+def get_file_by_char(char: str) -> int:
     num_in_alphabet = _get_char_number_in_alphabet(char.lower())
 
     if num_in_alphabet < 0 or num_in_alphabet > 7:
@@ -58,13 +62,14 @@ def get_file_by_char(char) -> int:
     return num_in_alphabet
 
 
-def get_rank_by_char(char) -> int:
+def get_rank_by_char(char: str) -> int:
     try:
         return int(char) - 1
     except ValueError:
         raise ValueError(char)
 
-def get_color_by_char(char) -> Color:
+
+def get_color_by_char(char: str) -> Color:
     if char == "w":
         return Color.WHITE
     elif char == "b":
@@ -72,9 +77,38 @@ def get_color_by_char(char) -> Color:
     else:
         raise ValueError(char)
 
+
 def get_char_by_color(color: Color) -> str:
     if color is Color.WHITE:
         return "w"
     elif color is Color.BLACK:
         return "b"
+
+
+def get_character_by_castling_move(move: CastlingMove):
+    if move.side is CastlingSide.KINGSIDE:
+        side_char = "k"
+    else:
+        side_char = "q"
+
+    if move.color is Color.WHITE:
+        return side_char.upper()
+    else:
+        return side_char.lower()
+
+
+def get_castling_move_by_character(char: str):
+    if char.isupper():
+        color = Color.WHITE
+    else:
+        color = Color.BLACK
+
+    side_char = char.lower()
+
+    if side_char == "k":
+        side = CastlingSide.KINGSIDE
+    else:
+        side = CastlingSide.QUEENSIDE
+
+    return CastlingMove(side, color)
 
